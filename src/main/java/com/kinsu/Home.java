3709bc16602f3887569feb31c03ce6c149a4e2f6
sujitkrjha <sujit.mca16.du@gmail.com>
2018-03-11 15:13:22 +0530

    Hello Programmer

---
+++ src/main/java/com/kinsu/Home.java
@@ -0,0 +1,14 @@
+package com.kinsu;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+	
+	@RequestMapping("/hello")
+	public String sayHello() {
+		return "Hello Programmer";
+	}
+
+}
